35de19f0e8ce969269f5fad9b300bf3eaaa3811f
Gravtas-J <144185093+Gravtas-J@users.noreply.github.com>
2024-03-30 15:25:47 +1000

    UPDATED: Removed code Removed extra lines unnecessary comments

--- app.py
+++ app.py
@@ -27,18 +27,13 @@ from modules.utils import open_file, Chatlog_loc, Content, portrait_path
 
 def main():
     st.set_page_config(layout="wide", page_title='Zara')
-
     load_dotenv()
-
-
     openai.api_key = os.getenv("OPENAI_API_KEY")
     startup()
     init_states() 
     show_msgs()
-    
     timeout_tasks()
     prompt = st.chat_input()
-    
     #============================CHATBOT FUNCTION =====================================#
     if prompt:
         st.session_state['has_timeout_run'] = "no"
@@ -57,28 +52,23 @@ def main():
             "content": time_right_now + Content  + memory
         }
         messages_for_api = [system_prompt] + st.session_state.messages
-        
         # Call the OpenAI API with the prepared messages, including the hidden system prompt.
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-0125",
             messages=messages_for_api
         )
         msg_content = response.choices[0].message["content"]
-        
         # Display assistant response in chat message container with streamed output
         with st.chat_message("assistant", avatar=portrait_path):
             st.write_stream(response_generator(msg_content))
-        
         # Add assistant response to chat history
         st.session_state.messages.append({"role": "assistant", "content": msg_content, })
         # Convert the chat log into a string, store it in the session state.
         chat_log = "<<BEGIN CHATLOG>>" +"\n".join([f"{msg['role'].title()}: {msg['content']}" for msg in st.session_state.messages])+ "<<END CHATLOG>>"
         st.session_state['chat_log'] = chat_log
-        
         # Append the latest user and assistant messages to the chatlog file
         append_to_chatlog(f"User: {prompt}")
         append_to_chatlog(f"Assistant: {msg_content}")
-
         current_Chatlog = open_file(Chatlog_loc)
         if len(current_Chatlog) > 2500: #TODO - I have to change this to a time based function that will split the chatlog into chunks of 2500 char then run each function. I want it to do it when there ahsn't been a response of about 5 min or soemthign so it happens when the user isn't there to experience the slowness
             write_journal()
