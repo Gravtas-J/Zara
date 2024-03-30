639a2d209d0ccf93787a289c25a29fefe13acf66
Gravtas-J <144185093+Gravtas-J@users.noreply.github.com>
2024-03-30 14:26:46 +1000

    UPDATED: modules/utils.py
    - Renamed the userprofile variable to user
    - the backup_user_matrix assignment above the User_matrix variable assignment
    - Fixed the typo in the portrait_path variable assignment
    - Updated the path to the Portrait folder in the portrait_path variable assignment

--- modules/utils.py
+++ modules/utils.py
@@ -11,14 +11,15 @@ def open_file(filepath):
 Chatlog_loc = os.path.join('app', 'Memories', 'chatlog.txt')
 Chatlog_loc = os.path.join('app', 'Memories', 'chatlog.txt')
 Journal_loc = os.path.join('app', 'Memories', 'Journal.txt')
-User_matrix = os.path.join('app', 'Memories', 'user_matrix.txt')
+
 userprofile=os.path.join('app', 'Memories', 'user_profile.txt')
-backup_user_matrix = os.path.join('app', 'Memories', 'user_matrix_backup.txt')
 backup_userprofile = os.path.join('app', 'Memories', 'user_profile_backup.txt')
+backup_user_matrix = os.path.join('app', 'Memories', 'user_matrix_backup.txt')
+User_matrix = os.path.join('app', 'Memories', 'user_matrix.txt')
 
 profile_template = open_file(os.path.join('modules', 'STARTUP', 'userprofile.txt'))
 matrix_template = open_file(os.path.join('modules', 'STARTUP', 'usermatrix.txt'))
-portrait_path = os.path.join('app', 'Protrait', 'T.png')
+
 
 chromadb_path = os.path.join('app','chromadb', 'chromaDB.db')
 
@@ -26,6 +27,7 @@ Journaler = os.path.join('app', 'system prompts', 'Journaler.md')
 Update_user = os.path.join('app', 'system prompts', 'User_update.md')
 Matrix_writer_prompt = os.path.join('app', 'system prompts', 'Personality_matrix.md')
 Persona=os.path.join('app', 'Personas', 'Zara.md')
+portrait_path = os.path.join('app', 'Portrait', 'T.png')
 
 Matrix_content = open_file(User_matrix)
 Matrix_writer_content = open_file(Matrix_writer_prompt)
